1ca036a0b0859fa11fa3148d2a363bc63d114ffe
Edward Le <edward.le.goldenowl@gmail.com>
2023-03-28 14:51:17 +0700

    chore: handle bad request exception error

--- src/auth/auth.controller.ts
+++ src/auth/auth.controller.ts
@@ -5,17 +5,20 @@ import {
   HttpCode,
   HttpStatus,
   UseInterceptors,
+  UseFilters,
 } from '@nestjs/common';
 
 import { IResponseBase } from '@interfaces/index';
+import { BadRequestExceptionFilter } from '@utils/BadRequestExceptionFilter';
 
 import AuthService from './auth.service';
 import { LoginInfoDTO } from './dto/auth.dto';
-import TransformInterceptor from './auth.interceptor';
 import { IAuthResponse } from './auth.interface';
+import TransformInterceptor from './auth.interceptor';
 
 @UseInterceptors(TransformInterceptor)
-@Controller('login')
+@UseFilters(new BadRequestExceptionFilter())
+@Controller({ path: 'login', version: ['1'] })
 export default class AuthController {
   constructor(private readonly authService: AuthService) {}
 
