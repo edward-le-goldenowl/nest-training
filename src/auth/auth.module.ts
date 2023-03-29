330eaac305a556c0d6925ea0b87904312fb00f35
Edward Le <edward.le.goldenowl@gmail.com>
2023-03-29 12:14:09 +0700

    chore: connect pg database

--- src/auth/auth.module.ts
+++ src/auth/auth.module.ts
@@ -1,9 +1,13 @@
 import { Module } from '@nestjs/common';
+import { TypeOrmModule } from '@nestjs/typeorm';
+
+import AccountEntity from '@models/account.entity';
+
 import AuthController from './auth.controller';
 import AuthService from './auth.service';
 
 @Module({
-  imports: [],
+  imports: [TypeOrmModule.forFeature([AccountEntity])],
   controllers: [AuthController],
   providers: [AuthService],
 })
--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -1,9 +1,18 @@
 import { HttpException, HttpStatus, Injectable } from '@nestjs/common';
+import { InjectRepository } from '@nestjs/typeorm';
+
+import AccountEntity from '@models/account.entity';
+
 import { ILoginData, IAuthResponse } from './auth.interface';
 import { LoginInfoDTO } from './dto/auth.dto';
+import { Repository } from 'typeorm';
 
 @Injectable()
 export default class AuthService {
+  constructor(
+    @InjectRepository(AccountEntity)
+    private accountRepository: Repository<AccountEntity>,
+  ) {}
   private loginDataMocked: ILoginData = {
     username: 'dungle',
     password: '111',
